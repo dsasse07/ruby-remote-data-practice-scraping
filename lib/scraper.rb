68b05d9496f68eedb621fdeff1b4b783dd8436bc
Daniel Sasse <dsasse07@gmail.com>
2020-12-22 13:53:30 -0500

    Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".site-header__hero__headline")
+
+# puts doc.css(".headline-26OIBN")
+class_titles = doc.css(".title-oE5vT4")
+class_titles.each {|title| puts title.text}
+
+binding.pry
+
+0
